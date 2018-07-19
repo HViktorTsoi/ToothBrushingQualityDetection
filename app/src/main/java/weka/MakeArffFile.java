32a300873ad671eb967a0b791fe9ca2b02a704ac
HViktorTsoi <cuijiahetony@163.com>
2018-07-19 22:41:57 +0800

    实现了将MFCC(12维度)写入标记数据文件

--- app/src/main/java/weka/MakeArffFile.java
+++ app/src/main/java/weka/MakeArffFile.java
@@ -22,7 +22,7 @@ public class MakeArffFile {
 
     private static FileWriter writer;
 
-    public static void initArffFile () {
+    public static void initArffFile() {
         try {
             File file = new File(Constant.FILE_PATH);
             boolean flag = true;
@@ -56,6 +56,19 @@ public class MakeArffFile {
                 writer.write("@attribute bFKur numeric\n");
                 writer.write("@attribute bFSke numeric\n");
                 writer.write("@attribute bFIqr numeric\n");
+                writer.write("@attribute MFCC1 numeric\n");
+                writer.write("@attribute MFCC2 numeric\n");
+                writer.write("@attribute MFCC3 numeric\n");
+                writer.write("@attribute MFCC4 numeric\n");
+                writer.write("@attribute MFCC5 numeric\n");
+                writer.write("@attribute MFCC6 numeric\n");
+                writer.write("@attribute MFCC7 numeric\n");
+                writer.write("@attribute MFCC8 numeric\n");
+                writer.write("@attribute MFCC9 numeric\n");
+                writer.write("@attribute MFCC10 numeric\n");
+                writer.write("@attribute MFCC11 numeric\n");
+                writer.write("@attribute MFCC12 numeric\n");
+                // 分类目标
                 writer.write("@attribute class {18,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}\n");
                 writer.write("\n");
                 writer.write("@data\n");
@@ -68,30 +81,35 @@ public class MakeArffFile {
 
 
     private static ArrayList<String> _record = new ArrayList<>();
+
     public static void calculate(List<Byte> datalist, String type, int sampleRateInHz, int channelConfig) {
         double[] param = new double[datalist.size()];
-        for(int i=0; i<datalist.size(); i++)
+        for (int i = 0; i < datalist.size(); i++)
             param[i] = Double.valueOf(datalist.get(i));
         double[] t_ret = AudioFeature.timedomain(param);
         double[] f_ret = AudioFeature.freqdomain(param);
-        List<float[]> mfcc_ret=AudioFeature.mfccFeature(datalist,sampleRateInHz,channelConfig);
+        List<float[]> mfcc_ret_batch = AudioFeature.mfccFeature(datalist, sampleRateInHz, channelConfig);
+
         //String tmp = "";
         try {
             writer = new FileWriter(Constant.FILE_PATH, true);
-            for (int i=0; i<t_ret.length; i++) {
-                writer.write( String.valueOf(t_ret[i]) + ",");
-                //tmp = tmp + String.valueOf(t_ret[i]) + ",";
+            for (double aT_ret : t_ret) {
+                writer.write(String.valueOf(aT_ret) + ",");
             }
-            for (int i=0; i<f_ret.length; i++) {
-                writer.write(String.valueOf(f_ret[i]) + ",");
-                //tmp = tmp + String.valueOf(f_ret[i]) + ",";
+            for (double aF_ret : f_ret) {
+                writer.write(String.valueOf(aF_ret) + ",");
+            }
+            // 如果有mfcc属性 则写入
+            if (mfcc_ret_batch != null && mfcc_ret_batch.size() > 0) {
+                // 由于窗口是自己实现好的 所以只需要写入mfccList的第一维 即当前窗口的mfcc
+                float[] mfcc_ret = mfcc_ret_batch.get(0);
+                for (float aMfcc_ret : mfcc_ret) {
+                    writer.write(String.valueOf(aMfcc_ret) + ",");
+                }
             }
             writer.write(type);
             writer.write("\n");
             writer.close();
-            //tmp = tmp + type + "\n";
-            //_record.add(tmp);
-            //writeToFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -102,7 +120,7 @@ public class MakeArffFile {
         if (_record.size() > 20) {
             try {
                 writer = new FileWriter(Constant.FILE_PATH, true);
-                for(String str:_record) {
+                for (String str : _record) {
                     writer.write(str);
                 }
                 _record.clear();
@@ -112,7 +130,6 @@ public class MakeArffFile {
             }
         }
     }
-
 
 
 }
